c695a4d28cc2d0513235e28460eb840eea95647b
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2026-02-24 22:03:02 +0530

    Setup Ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Akash Khandagale",
+    author_email="akashkhandagale038@gmail.com",
+    packages=find_packages()
+)
